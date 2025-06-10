1d38cf44d8a993ae2e9b5b5c5af073047bc2cc8d
Haikalimanf <haikalimanf@gmail.com>
2025-06-10 13:27:07 +0700

    First Commit Tamplate UI

---
+++ app/src/main/java/com/example/seacatering/MyApp.kt
@@ -0,0 +1,7 @@
+package com.example.seacatering
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application()
---
+++ app/src/main/java/com/example/seacatering/ui/subcription/SubscriptionViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.seacatering.ui.subcription
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SubscriptionViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
